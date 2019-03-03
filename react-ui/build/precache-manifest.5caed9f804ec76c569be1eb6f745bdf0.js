1f36557c7c234e032d0d1d6d42e4e5e31717f0f0
denisegiubilei <giubileidenise@gmail.com>
2019-03-03 22:25:50 +0100

    one more try to get heroku to work

---
+++ react-ui/build/precache-manifest.5caed9f804ec76c569be1eb6f745bdf0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "de9172ea7ee6214e2af7397744fad7b4",
+    "url": "/static/media/stop.de9172ea.png"
+  },
+  {
+    "revision": "2571b21bcb8bc9796384",
+    "url": "/static/css/main.2019b9dc.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "547de6f397760cd8f8c6b92ec65889f6",
+    "url": "/static/media/quit-background.547de6f3.jpg"
+  },
+  {
+    "revision": "7214a2888f4408d46a5f",
+    "url": "/static/js/2.7214a288.chunk.js"
+  },
+  {
+    "revision": "2571b21bcb8bc9796384",
+    "url": "/static/js/main.2571b21b.chunk.js"
+  },
+  {
+    "revision": "90653a8162a11f96de83e4467c18fa59",
+    "url": "/static/media/time.90653a81.gif"
+  },
+  {
+    "revision": "7bcba55187df137b234567a0337e27df",
+    "url": "/static/media/money.7bcba551.gif"
+  },
+  {
+    "revision": "bd69bcfa1b185704ffe5ba24a2443c08",
+    "url": "/static/media/cigars.bd69bcfa.gif"
+  },
+  {
+    "revision": "7214a2888f4408d46a5f",
+    "url": "/static/css/2.b4a3d8ba.chunk.css"
+  },
+  {
+    "revision": "8ae4e9a4c22443d278958a987cf56d43",
+    "url": "/index.html"
+  }
+];
