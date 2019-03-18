d51330f359c3247b2ab3b0b8fb85622048128c7e
denisegiubilei <giubileidenise@gmail.com>
2019-03-18 01:09:15 +0100

    Fix: preventing keyboard on datepicker

--- react-ui/src/components/QuitForm/QuitForm.js
+++ react-ui/src/components/QuitForm/QuitForm.js
@@ -7,48 +7,50 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 class QuitForm extends React.Component {
 
-  handleDateChangeRaw = (e) => {
-    e.preventDefault();
+  onDatepickerRef(el) { 
+    if (el && el.input) { 
+      el.input.readOnly = true; 
+    } 
   }
 
   render() {
     return (
-        <div className="quit-form">
-          <Form>
-            <Form.Group controlId="quit-date">
-              <DatePicker
-                  placeholderText="The day I quit"
-                  selected={this.props.quitData.date}
-                  onChange={this.props.handleChangeDate}
-                  onChangeRaw={this.handleDateChangeRaw}
-                  className="form-control"
-                  showTimeSelect
-                  timeFormat="HH:mm"
-                  timeIntervals={60}
-                  dateFormat="MMMM d, yyyy h:mm aa"
-                  timeCaption="time"
-                />
-            </Form.Group>
-            <Form.Group controlId="packs-per-week">
-              <Form.Control 
-                name="packsPerWeek"
-                type="number" 
-                placeholder="Packs per Week" 
-                value={this.props.quitData.packsPerWeek}
-                onChange={this.props.handleInputChange}
-              />
-            </Form.Group>
-            <Form.Group controlId="price-per-pack">
-              <Form.Control 
-                name="pricePerPack"
-                type="number" 
-                placeholder="Price per pack" 
-                value={this.props.quitData.pricePerPack}
-                onChange={this.props.handleInputChange}
-              />
-            </Form.Group>
-          </Form>
-        </div>
+      <div className="quit-form">
+        <Form>
+          <Form.Group controlId="quit-date">
+            <DatePicker
+              placeholderText="The day I quit"
+              selected={this.props.quitData.date}
+              onChange={this.props.handleChangeDate}
+              ref={el => this.onDatepickerRef(el)}
+              className="form-control"
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={60}
+              dateFormat="MMMM d, yyyy h:mm aa"
+              timeCaption="time"
+            />
+          </Form.Group>
+          <Form.Group controlId="packs-per-week">
+            <Form.Control
+              name="packsPerWeek"
+              type="number"
+              placeholder="Packs per Week"
+              value={this.props.quitData.packsPerWeek}
+              onChange={this.props.handleInputChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="price-per-pack">
+            <Form.Control
+              name="pricePerPack"
+              type="number"
+              placeholder="Price per pack"
+              value={this.props.quitData.pricePerPack}
+              onChange={this.props.handleInputChange}
+            />
+          </Form.Group>
+        </Form>
+      </div>
     )
   }
 }
